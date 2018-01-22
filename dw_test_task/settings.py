7e9e7f0431bc56d36ac0233ea97ca4917c07d27f
Skochynskiy Dmytro <skodmy@gmail.com>
2018-01-22 20:50:20 +0200

    small fixes

--- dw_test_task/settings.py
+++ dw_test_task/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'x76buo4$f36kv0x+6@acp$bp=90u%b@p3)x1&ioury0-2$)^pg'  # not secure
+SECRET_KEY = 'x76buo4$f36kv0x+6@acp$bp=90u%b@p3)x1&ioury0-2$)^pg'  # not production secret
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
